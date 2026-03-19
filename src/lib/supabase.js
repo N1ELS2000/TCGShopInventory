60c660259fd50d09fa025052e6e12defed466c96
N1ELS2000 <niels.vanderplanken@student.uantwerpen.be>
2026-03-19 02:01:16 +0100

    supabase creds

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://oivyotdqysmhtukijqzf.supabase.co";
-const supabaseAnonKey = "sb_publishable_8_3kK7-NN5ER6dSZUKv3WQ_jiQdRGnY";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
